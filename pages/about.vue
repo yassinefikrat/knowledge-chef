51e8de10733a805031020e170076409a331a8ad0
Yassine Fikrat <yassinefikrat@gmail.com>
2019-07-11 19:48:40 +0200

    feat: import and display a buds-ui button

--- pages/about.vue
+++ pages/about.vue
@@ -1,26 +1,39 @@
 <template>
   <section>
-    <div>
-      <Title/>
+    <div id="app">
       <div class="links">
         <nuxt-link to="/">Go to home</nuxt-link>
       </div>
+      <ChuccButton @click="processClickOnNew">New</ChuccButton>
     </div>
   </section>
 </template>
 
 <script>
-import Title from '~/components/Title.vue'
+import ChuccButton from '@bit/yassinefikrat.buds-ui.chucc-button'
 
 export default {
   components: {
-    Title
+    ChuccButton
+  },
+  methods: {
+    processClickOnNew: function() {
+      alert('Clicked on New')
+    }
   }
 }
 </script>
 
 <style>
 .links {
-  padding-top: 15px;
+  margin-top: 60px;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #FFE5D9;
 }
 </style>
